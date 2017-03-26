45f8221132bdd5e1ba16726fa6c36b42d3a2df1b
espresso713 <tutuc713@gmail.com>
2017-03-26 21:13:46 +0900

    firt commit

---
+++ src/main/java/exam/javaTest.java
@@ -0,0 +1,10 @@
+package exam;
+
+/**
+ * Created by hyung on 2017-03-20.
+ */
+public class javaTest {
+    public static void main(String[] args) {
+        System.out.println("javaTest run");
+    }
+}
